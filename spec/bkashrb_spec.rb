84f879fffe7dc936b3c7924615002b411ca598e9
Irfan Ahmed <irfandhk@gmail.com>
2021-11-28 22:24:48 +0100

    Initial code

---
+++ spec/bkashrb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bkashrb do
+  it "has a version number" do
+    expect(Bkashrb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
